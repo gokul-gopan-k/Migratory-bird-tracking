4857f1b649f06bd3edb104cd53d0effdb64c6dc4
gokul-gopan-k <gokul.gopan.k@gmail.com>
2024-11-09 13:21:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,53 +8,64 @@ import pickle
 import numpy as np
 from pydub import AudioSegment
 import librosa
-# Path to the downloaded shapefile (replace with your local path)
+
+
+# Path to the downloaded shapefile 
 shapefile_path = "map/ne_110m_admin_0_countries.shp"
 
-# Sample countries to highlight
-countries_to_highlight = ["United States of America", "Brazil", "India"]
-data_csv =pd.read_csv("Birds Voice.csv")
-model_3 = tf.keras.models.load_model('yamnet_classifier_model.h5')
-with open('label_encoder.pkl', 'rb') as f:
-    label_encoder_3 = pickle.load(f)
+# Bird audio data file
+birdData_csv = pd.read_csv("Birds Voice.csv")
 
-# Load YAMNet model
+# Load trained model
+model = tf.keras.models.load_model('yamnet_classifier_model.h5')
+
+# Load label encoder
+with open('label_encoder.pkl', 'rb') as f:
+    label_encoder = pickle.load(f)
+
+# Load YAMNet model for inference
 yamnet_model_handle = "https://tfhub.dev/google/yamnet/1"
 yamnet_model = hub.load(yamnet_model_handle)
 
 
 def load_audio_file(filepath):
-    y, sr = librosa.load(filepath, sr=16000)  # Load with 16kHz sampling rate
-    print(f"Audio data shape: {y.shape}")
-    return y
+    # Load with 16kHz sampling rate
+    audio, sample_rate = librosa.load(filepath, sr=16000)  
+    return audio
+
 def yamnet_embeddings(audio_data):
     audio_data = np.squeeze(audio_data) 
     scores, embeddings, spectrogram = yamnet_model(audio_data)
-    return embeddings.numpy()  # Shape: (num_patches, 1024)
+    return embeddings.numpy()  
 
-def test_inf(audio):
+def prepare_testData(audio):
     sound = AudioSegment.from_mp3(audio)
-    sound = sound.set_frame_rate(16000)  # Resample to 16kHz
-    sound = sound.set_channels(1)        # Convert to mono
+    # Resample to 16kHz
+    sound = sound.set_frame_rate(16000)  
+    # Convert to mono
+    sound = sound.set_channels(1)        
     sound.export("wav_test/w_1", format="wav")
     # Prepare the dataset by extracting embeddings
     all_embeddings = []
     audio_data = load_audio_file("wav_test/w_1")
-    audio_data = np.reshape(audio_data, (1, -1))  # Reshape for batch dimension
+     # Reshape for batch dimension
+    audio_data = np.reshape(audio_data, (1, -1)) 
     embeddings = yamnet_embeddings(audio_data)
-    all_embeddings.append(np.mean(embeddings, axis=0))  # Use mean embedding
-    X = np.array(all_embeddings) 
-    predd = model_3.predict(X)
-    predd_class_indices = predd.argmax(axis=1)
+    # Use mean embedding
+    all_embeddings.append(np.mean(embeddings, axis=0))  
+    X_test = np.array(all_embeddings) 
+    pred = model.predict(X_test)
+    pred_class_indices = pred.argmax(axis=1)
     # Now pass these indices to inverse_transform
-    return label_encoder_3.inverse_transform(predd_class_indices)[0]
+    return label_encoder.inverse_transform(pred_class_indices)[0]
 
 
 # Create world map using Plotly
 def create_map(audio,country_spotted):
-
-    c_name = data_csv[data_csv.common_name == test_inf(audio)].scientific_name.iloc[0]
-    countries_seen = list(data_csv[data_csv.common_name == test_inf(audio)].Country.unique())
+    # Get scientific name
+    sc_name = birdData_csv [birdData_csv .common_name == prepare_testData(audio)].scientific_name.iloc[0]
+    # Get countries seen
+    countries_seen = list(birdData_csv [birdData_csv .common_name == prepare_testData(audio)].Country.unique())
     
     # Load the world map from the shapefile
     world = gpd.read_file(shapefile_path)
@@ -72,23 +83,36 @@ def create_map(audio,country_spotted):
     )
     # Increase map size and reduce legend size
     fig.update_layout(
-        width=800,  # Map width
-        height=500,  # Map height
+        # Map width
+        width=800,  
+        # Map height
+        height=500,  
         legend=dict(
-            orientation="h",   # Horizontal legend
-            yanchor="top",     # Align legend at the top
-            y=-0.2,            # Position legend below the map
-            xanchor="center",  # Center align legend
-            x=0.5,             # Center it horizontally
-            font=dict(size=10)  # Legend font size
+            # Horizontal legend
+            orientation="h",   
+            # Align legend at the top
+            yanchor="top",    
+            # Position legend below the map 
+            y=-0.2,         
+            # Center align legend   
+            xanchor="center",  
+            # Center it horizontally
+            x=0.5,      
+            # Legend font size       
+            font=dict(size=10)  
+        )      
         )
-       
-        )
+    # Generate analysis
     if country_spotted in countries_seen:
         analysis = "Bird spotted in its expected country."
+        color = "lightgreen"
+        alert_flag = " Spotted in its expected migratory route"
     else:
         analysis = "Bird spotted in a country outside its migratory path. There is likely change in migratory path due to reasons such as climate change or deforestation."
-    return fig,test_inf(audio), c_name, countries_seen, analysis
+        color = "#FF073A"
+        alert_flag = "ALERT: Spotted in a new country"
+    alert_html = f"<span style='color: {color}; font-size: 20px'>{alert_flag}</span>"    
+    return fig,prepare_testData(audio), sc_name, countries_seen, alert_html, analysis
 
 # Create Gradio interface
 iface = gr.Interface(
@@ -97,7 +121,7 @@ iface = gr.Interface(
              gr.Textbox(value="Japan", label="Enter Country of Spotting"),
             ],
     outputs=[gr.Plot(), gr.Textbox(value="Andean", label="Audio belongs to the bird"), gr.Textbox(value="Andean", label="Scientific name is"), 
-             gr.Textbox(value="India, Australia", label="Countries of Migration are"),
+             gr.Textbox(value="India, Australia", label="Countries of Migration are"),  gr.HTML(label="Alert"), 
              gr.Textbox(value="Bird sighted in an expected country", label="Analysis")
             ],
     examples = [["audio_inputs/Andean Tinamou2.mp3"],["audio_inputs/Andean Guan8.mp3"],[ "audio_inputs/Common Ostrich2.mp3"]],
